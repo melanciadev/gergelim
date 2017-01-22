1c1560f09b3d1d8e5eac0af873ce396369c72ce7
Unknown <unphook@gmail.com>
2017-01-22 07:17:39 -0200

    começando a montar ciclo de dia e noite

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Melancia.Gergelim {
+	public class Map:MonoBehaviour {
+		public static float time;
+		float timeStart;
+		//0: meia noite
+		//32: meio dia
+		//64: meia noite dnv (volta pro 0)
+
+		const float duration = 120;
+		const float usedDuration = 64;
+
+		void Awake() {
+			//
+		}
+
+		void Start() {
+			//
+		}
+
+		void Update() {
+			time += Time.deltaTime*(usedDuration/duration);
+			while (time >= 64) time -= 64;
+			// 
+		}
+	}
+}
