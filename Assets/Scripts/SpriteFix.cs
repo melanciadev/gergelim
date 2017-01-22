3e2ae4cf620ed950cc1d50a34e1944e840c0cd35
Unknown <unphook@gmail.com>
2017-01-22 09:20:07 -0200

    dor

---
+++ Assets/Scripts/SpriteFix.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spriter2UnityDX;
+
+namespace Melancia.Gergelim {
+	public class SpriteFix:MonoBehaviour {
+		Transform tr;
+		SpriteRenderer spriteRenderer;
+		EntityRenderer entityRenderer;
+		float z;
+
+		void Awake() {
+			tr = transform;
+			spriteRenderer = GetComponent<SpriteRenderer>();
+			entityRenderer = GetComponent<EntityRenderer>();
+			UpdateDepth();
+		}
+
+		void FixedUpdate() {
+			if (z != tr.position.z) UpdateDepth();
+		}
+
+		void UpdateDepth() {
+			z = tr.position.z;
+			int order = -(int)((z+6)*100);
+			if (spriteRenderer != null) spriteRenderer.sortingOrder = order;
+			if (entityRenderer != null) entityRenderer.SortingOrder = order;
+		}
+	}
+}
